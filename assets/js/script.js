b21e4d6f234598c8360d72d494d9f276153ee7f9
abdullahjr0 <abdullah.rafique01@gmail.com>
2021-08-21 23:24:07 +0000

    add js for overlay and cards

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+const cards = document.querySelectorAll(".memory-card");
+const time = document.querySelector(".time");
+const reload = document.querySelector(".reload");
+const timers = document.querySelector(".calculate");
+const overlay = document.querySelector(".hide");
+const overlay2 = document.querySelector(".hide2");
+const play = document.querySelector(".play");
+const play2 = document.querySelector(".play2");
