c48c261446f2d1814d4c5e86974abf38cce672ee
abdullahjr0 <abdullah.rafique01@gmail.com>
2021-08-21 23:27:00 +0000

    add flip card function

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,3 +55,22 @@ function reset() {
     reset();
     clock = true;
   });
+
+  function flipCard() {
+    if (disply) return;
+    if (lockBoard) return;
+    if (this === firstCard) return;
+  
+    this.classList.add("flip");
+    click++;
+    time.textContent = click;
+    if (!hasFlippedCard) {
+      hasFlippedCard = true;
+      firstCard = this;
+  
+      return;
+    }
+  
+    secondCard = this;
+    checkForMatch();
+  }
