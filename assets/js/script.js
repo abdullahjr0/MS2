99ac7d0a45632f2176ba2ce5eb84a44318e6faeb
abdullahjr0 <abdullah.rafique01@gmail.com>
2021-08-21 23:24:26 +0000

    add js for timer and clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,22 @@ const overlay = document.querySelector(".hide");
 const overlay2 = document.querySelector(".hide2");
 const play = document.querySelector(".play");
 const play2 = document.querySelector(".play2");
+let hasFlippedCard = false;
+let lockBoard = false;
+let disply = false;
+let firstCard, secondCard;
+let clock = true;
+let number = 0;
+let click = 0;
+let timer = 60;
+setInterval(() => {
+  if (clock) {
+    timer--;
+    if (timer === 0) {
+      timers.innerHTML = 0;
+      overlay.style.visibility = "visible";
+    } else if (timer >= 1) {
+      timers.innerHTML = timer;
+    }
+  }
+}, 1000);
