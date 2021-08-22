d0669c46f7b48e48b0b802fef4dd1b0bc5fc91a5
abdullahjr0 <abdullah.rafique01@gmail.com>
2021-08-22 02:14:04 +0000

    add js2 for semi pro tier

---
+++ assets/js/script2.js
@@ -0,0 +1,29 @@
+"use strict";
+const cards = document.querySelectorAll(".memory-card");
+const time = document.querySelector(".time");
+const reload = document.querySelector(".reload");
+const timers = document.querySelector(".calculate");
+const overlay = document.querySelector(".hide");
+const overlay2 = document.querySelector(".hide2");
+const play = document.querySelector(".play");
+const play2 = document.querySelector(".play2");
+let hasFlippedCard = false;
+let lockBoard = false;
+let disply = false;
+let firstCard, secondCard;
+let clock = true;
+let number = 0;
+let click = 0;
+let timer = 60;
+setInterval(() => {
+  if (clock) {
+    timer--;
+    if (timer === 0) {
+      timers.innerHTML = 0;
+      overlay.style.visibility = "visible";
+    } else if (timer >= 1) {
+      timers.innerHTML = timer;
+    }
+  }
+}, 1000);
+
